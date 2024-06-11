ad8aa012503851bc6275dd529de6a6e96b8aefdb
P MANVITHA RAYAL <113161233+manvithapula@users.noreply.github.com>
2024-06-11 22:44:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('#search');
+const resultsList = document.querySelector('#results');
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchRecipes();
+})
+
+async function searchRecipes() {
+    const searchValue = searchInput.value.trim();
+    const response = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=f4bf8a84&app_key=c56f7a6a4e9f86c6786cd99938f2fd72&from=0&to=10`);
+    const data = await response.json();
+    displayRecipes(data.hits);
+}
+
+function displayRecipes(recipes) {
+    let html = '';
+    recipes.forEach((recipe) => {
+        html += `
+        <div>
+            <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+            <h3>${recipe.recipe.label}</h3>
+            <ul>
+                ${recipe.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
+            </ul>
+            <a href="${recipe.recipe.url}" target="_blank" class="button">View Recipe</a>
+        </div> 
+        `
+    })
+    resultsList.innerHTML = html;
+}
